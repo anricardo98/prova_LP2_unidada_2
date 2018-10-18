69271fea3e538727968f46348f997d8120d4c5c5
Leonardo Bezerra <leobezerra@imd.ufrn.br>
2018-10-18 20:12:27 -0300

    [Sorting] Atualização dos nomes dos arquivos. [Tree] Primeiro commit.

--- sorting/sort.py
+++ sorting/sort.py
@@ -1,8 +1,8 @@
 from contextlib import redirect_stdout
 
-with open("collation.in", encoding="utf-8") as f:
+with open("rotulos.in", encoding="utf-8") as f:
     ordered = sorted([line.strip() for line in f.readlines()])
 
-with open("collation.out", "w", encoding="utf-8") as f:
+with open("rotulos.out", "w", encoding="utf-8") as f:
     with redirect_stdout(f):
         print("\n".join(ordered))
